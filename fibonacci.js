387ba74521ed0a7997db05afc10b9fb9e9fdcfcf
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-05-10 11:41:31 +0300

    add fibonacci solution

---
+++ fibonacci.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+function fibs(n) {
+  let fibArray = [0, 1];
+  for (let i = 1; i < n - 1; i++) {
+    fibArray.push(fibArray[i] + fibArray[i - 1]);
+  }
+  console.log(fibArray);
+}
+
+function fibsRec(n) {
+  return n === 1
+    ? [0]
+    : n === 2
+    ? [0, 1]
+    : [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]];
+}
+console.log(fibsRec(8));
