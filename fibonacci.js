abd726caa9a99c6171244a0c5b0113038ab189e3
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2024-05-10 11:44:58 +0300

    change fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -15,4 +15,9 @@ function fibsRec(n) {
     ? [0, 1]
     : [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]];
 }
-console.log(fibsRec(8));
+
+//To test the iterative fibonacci function  uncomment below
+/* console.log("Iterative: " + fibs(7)); */
+
+//To test the recursive fibonacci function  uncomment below
+/* console.log("Recursive:" + fibsRec(8)); */
